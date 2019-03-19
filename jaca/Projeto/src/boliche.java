886a4e510ef1fd212121797abee16c8d02df3bd0
Logon Aluno <logonrmlocal@fiap.com.br>
2019-03-19 10:33:31 -0300

    adição da pasta jaca

---
+++ jaca/Projeto/src/boliche.java
@@ -0,0 +1,48 @@
+public class boliche {
+	String corBola;
+	double raio;
+
+	public boliche(String corbola, double tamanho){
+	this.corBola = corbola;
+	this.raio = tamanho;
+	}
+	//não é nescessario
+	public boliche() {
+		
+	this(null, 0);
+	}
+	public boliche maiorBola(boliche bola2, boliche bola3) {
+		//essa é a bola 1 
+		boliche maior = this;
+		if(bola2.raio > this.raio && bola2.raio > bola3.raio){
+			maior = bola2;
+		}else {
+			if(this.raio > bola2.raio && this.raio > bola3.raio) {
+				maior = this;
+			}else {
+				maior = bola3;
+			}
+		}
+		
+		return maior;
+		
+	
+	}
+
+
+public String retornarDados(){
+	String aux = "";
+	
+	aux = corBola+" "+raio;
+	
+	return aux;                                    
+}
+
+
+
+
+
+
+
+
+}
